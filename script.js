7283b78b04459e1a08d5726fbaa4332def1d7c44
Saba <saba.sadegh67@gmail.com>
2022-10-24 13:15:45 +0330

    add some intial logic for game to js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-git remote add origin https://github.com/zaberzz/guess-my-number.git
+"use strict";
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ NO NUMber";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🏆 You Win the game";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lose the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score < 1) {
+      document.querySelector(".message").textContent = "📈 Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
+  } else {
+    document.querySelector(".message").textContent = "You lose the game";
+    document.querySelector(".score").textContent = 0;
+  }
+});
